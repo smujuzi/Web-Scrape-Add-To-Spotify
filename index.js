b514a89b1a7663109ba23ea31e4b660ea3d9ea3e
STUART Kasekende <smk088@cs.bham.ac.uk>
2022-06-19 10:41:22 +0100

    swapped back to lambda

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const spotifyFeatures = require("./spotify/features");
 const collectSongs = require("./song_matching");
 
-async function generateRecommendedSongsPlaylist() {
-  console.log("SHOW TIME");
-  recommendedSongs = await collectSongs.getTopTracks();
-  await spotifyFeatures.createTopTracksPLaylist(recommendedSongs);
-  console.log("DONE");
-}
-generateRecommendedSongsPlaylist();
-
-module.exports = {
-  generateRecommendedSongsPlaylist,
-};
-
-//LAMBDA FUNCTION
-
-// exports.handler = async (event) => {
+// async function generateRecommendedSongsPlaylist() {
 //   console.log("SHOW TIME");
 //   recommendedSongs = await collectSongs.getTopTracks();
 //   await spotifyFeatures.createTopTracksPLaylist(recommendedSongs);
 //   console.log("DONE");
+// }
+// generateRecommendedSongsPlaylist();
+
+// module.exports = {
+//   generateRecommendedSongsPlaylist,
 // };
+
+//LAMBDA FUNCTION
+
+exports.handler = async (event) => {
+  console.log("SHOW TIME");
+  recommendedSongs = await collectSongs.getTopTracks();
+  await spotifyFeatures.createTopTracksPLaylist(recommendedSongs);
+  console.log("DONE");
+};
 
 // exports.handler = async (event) => {
 //   // TODO implement
