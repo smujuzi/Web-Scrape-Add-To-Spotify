f8df11b8ab08e74726853b586f28bdd4e0f7685c
Stuart <stuartkasekende1@gmail.com>
2023-02-12 14:13:10 +0000

    small edit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const spotifyAPI = require("../spotify/setup");
 
 exports.handler = async (event, context, callback) => {
   const spotifyAPIHand = await spotifyAPI.setupAPI();
+  const t = "hello";
 
   const recommendedSongs = await collectSongs.getTopTracks(spotifyAPIHand);
 
